00989953470bdd69b80db79112e6eaac2d46502f
Arnaud-pauli <53037481+Arnaud-pauli@users.noreply.github.com>
2019-08-05 13:13:02 +0200

    Update opendlv-device-hedgehog.cpp

--- opendlv-device-hedgehog/opendlv-device-hedgehog.cpp
+++ opendlv-device-hedgehog/opendlv-device-hedgehog.cpp
@@ -1,1 +1,9 @@
-a
+#include <ncurses.h>
+#include <poll.h>
+#include <thread>
+#include <sys/stat.h> // checking if file/dir exist
+#include <fstream>
+#include <string>
+
+#include "opendlv-standard-message-set.hpp"
+#include "automower_hrp.json"
