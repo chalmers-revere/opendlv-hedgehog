d754eef1f0e6a1c47290852fe215f0cc6e32b664
Arnaud-pauli <53037481+Arnaud-pauli@users.noreply.github.com>
2019-08-10 15:32:47 +0200

    Update opendlv-gw-hedgehog.cpp

--- opendlv-gw-hedgehog/src/opendlv-gw-hedgehog.cpp
+++ opendlv-gw-hedgehog/src/opendlv-gw-hedgehog.cpp
@@ -1,32 +1,85 @@
-#include <iostream>
-#include <thread>
-#include <cstring>
-
-#include <zmq.h>
-#include <json.hpp>
-
 #include "cluon-complete.hpp"
 #include "opendlv-standard-message-set.hpp"
+
+#include <linux/joystick.h>
+#include <fcntl.h>
+#include <sys/ioctl.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#include <cerrno>
+#include <cmath>
+#include <cstdint>
+#include <cstring>
+#include <iostream>
+#include <mutex>
+#include <string>
+#include <thread>
+
 
 int32_t main(int32_t argc, char **argv)
 {
   int32_t retCode{0};
   auto commandlineArguments = cluon::getCommandlineArguments(argc, argv);
-  if (0 == commandlineArguments.count("cid") || 0 == commandlineArguments.count("port") || 0 == commandlineArguments.count("http-root")) {
-    std::cerr << argv[0] << " is the the graphical interface for the OpenDLV simulation environment." << std::endl;
-    std::cerr << "Usage:   " << argv[0] << " --cid=<libcluon session> --port=<the port where HTTP/WebSocket is served> --http-root=<folder where HTTP content can be found> [--id=<Identifier in case of multiple running instances>] [--verbose]" << std::endl;
-    std::cerr << "Example: " << argv[0] << " --cid=111 --port=8000 --http-root=./http" << std::endl;
-    retCode = 1;
-  } else {
-    bool const VERBOSE{commandlineArguments.count("verbose") != 0};
-    uint16_t const CID = static_cast<uint16_t>(std::stoi(commandlineArguments["cid"]));
-
-    uint32_t const HTTP_PORT = static_cast<uint32_t>(std::stoi(commandlineArguments["port"]));
-    std::string const HTTP_ROOT = commandlineArguments["http-root"];
+  if ( (0 == commandlineArguments.count("cid")) ||
+     (0 == commandlineArguments.count("device")) ||
+     (0 == commandlineArguments.count("freq")) ||
+     (0 == commandlineArguments.count("axis-left-updown")) ||
+     (0 == commandlineArguments.count("axis-right-updown")) ) {
+   std::cerr << argv[0] << " interfaces with the given PS3 controller to emit ActuationRequest messages to an OD4Session." << std::endl;
+   retCode = 1;
+  }
+  else {
+    const int32_t MIN_AXES_VALUE = -32768;
+    const uint32_t MAX_AXES_VALUE = 32767;
     
-    std::string const SSL_CERT_PATH{(commandlineArguments["ssl-cert-path"].size() != 0) ? commandlineArguments["ssl-cert-path"] : ""};
-    std::string const SSL_KEY_PATH{(commandlineArguments["ssl-key-path"].size() != 0) ? commandlineArguments["ssl-key-path"] : ""};
+    const bool VERBOSE{commandlineArguments.count("verbose") != 0};
+    
+      cluon::OD4Session od4{static_cast<uint16_t>(std::stoi(commandlineArguments["cid"]))};
+      if (od4.isRunning()) {
+        od4.timeTrigger(FREQ, [&VERBOSE, &valuesMutex, &left, &right, &hasError, &od4](){
+            std::lock_guard<std::mutex> lck(valuesMutex);
 
+
+            opendlv::proxy::PedalPositionRequest pprl;
+            pprl.position(left);
+            od4.send(pprl, cluon::time::now(), 0);
+
+            opendlv::proxy::PedalPositionRequest pprr;
+            pprr.position(right);
+            od4.send(pprr, cluon::time::now(), 10);
+
+            return !hasError;
+            });
+
+        opendlv::proxy::PedalPositionRequest ppr;
+        ppr.position(0.0);
+        od4.send(ppr, cluon::time::now(), 0);
+        od4.send(ppr, cluon::time::now(), 10);
+      }
+
+      {
+        std::lock_guard<std::mutex> lck(valuesMutex);
+        hasError = true;
+        gamepadReadingThread.join();
+      }
+
+      ::close(gamepadDevice);
+      retCode = 0;
+    }
+  }
+  return retCode;
+}
+
+
+    
+    
+    
+    
+    
+  
+    
 int16_t leftwheelmotor=0; //-100 (max reverse), 100 (max forwards) 
 int16_t rightwheelmotor=0; //-100 (max reverse), 100 (max forwards) 
 
